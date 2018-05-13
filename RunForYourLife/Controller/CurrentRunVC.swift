80283793c29e3829a25b2564a1619643e6213a5b
Valentinas Mirosnicenko <valentinasmirosnicenko@Valentinass-MacBook-Pro-5.local>
2018-05-13 14:41:12 +0800

    Lesson 7 Completed

--- RunForYourLife/Controller/CurrentRunVC.swift
+++ RunForYourLife/Controller/CurrentRunVC.swift
@@ -8,28 +8,65 @@
 
 import UIKit
 
-class CurrentRunVC: UIViewController {
+class CurrentRunVC: LocationVC, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var avgPaceLabel: UILabel!
+    
+    @IBOutlet weak var swipeBGImageView: UIImageView!
+    @IBOutlet weak var sliderImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped(sender:)))
+        sliderImageView.addGestureRecognizer(swipeGesture)
+        sliderImageView.isUserInteractionEnabled = true
+        swipeGesture.delegate = self
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func pauseRunButtonPressed(_ sender: Any) {
+        
     }
-    */
-
+    
+    @objc func endRunSwiped(sender: UIPanGestureRecognizer) {
+        let minAdjust: CGFloat = 60
+        
+        let maxAdjust: CGFloat = 100
+        
+        if let sliderView = sender.view {
+            if sender.state == UIGestureRecognizerState.began || sender.state == UIGestureRecognizerState.changed {
+                
+                let translation = sender.translation(in: self.view)
+                
+                if sliderView.center.x >= (swipeBGImageView.center.x - minAdjust) && sliderView.center.x <= (swipeBGImageView.center.x + maxAdjust) {
+                    sliderView.center = CGPoint(x: sliderView.center.x + translation.x, y: sliderView.center.y)
+                } else if sliderView.center.x >= (swipeBGImageView.center.x + maxAdjust) {
+                    sliderView.center.x = (swipeBGImageView.center.x + maxAdjust)
+                    
+                    // End run
+                    
+                    dismiss(animated: true, completion: nil)
+                    
+                } else if sliderView.center.x <= (swipeBGImageView.center.x - minAdjust) {
+                    sliderView.center.x = (swipeBGImageView.center.x - minAdjust)
+                }
+                
+                sender.setTranslation(CGPoint.zero, in: self.view)
+                
+                
+            } else if sender.state == UIGestureRecognizerState.ended {
+                UIView.animate(withDuration: 0.15) {
+                    sliderView.center.x = self.swipeBGImageView.center.x - minAdjust
+                }
+            }
+        }
+        
+        
+    }
+    
+    
 }
