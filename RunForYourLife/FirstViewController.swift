f028fd56dec7435d9e0d95cc5cfd010607cd5b62
Valentinas Mirosnicenko <valentinasmirosnicenko@valentinass-macbook-pro-5.local>
2018-05-13 11:52:52 +0800

    Initial Commit

---
+++ RunForYourLife/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RunForYourLife
+//
+//  Created by Valentinas Mirosnicenko on 5/13/18.
+//  Copyright © 2018 Valentinas Mirosnicenko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
